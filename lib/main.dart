aa2ac0e6c0fc68030fd54e94e6e327f0772ea5af
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-12 16:32:54 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Center(child: Text("hello"))));
+  runApp(const MaterialApp(home: Center(child: Text("hello world"))));
 }
