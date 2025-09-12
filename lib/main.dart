63636c2490661a69083faf99c8304f1efc7dc7be
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-12 18:09:20 +0500

    Added & setup the assets

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ void main() {
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-              'https://plus.unsplash.com/premium_photo-1672116453187-3aa64afe04ad?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            ),
+            image: AssetImage('assets/two.png'),
           ),
         ),
       ),
