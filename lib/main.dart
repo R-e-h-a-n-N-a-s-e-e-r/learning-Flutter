16d9cba41cedbe5b28122dcd8ba595aaf0545903
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-12 20:05:53 +0500

    learnt Class,StatelessWidget and Hot Reloading

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amber.shade900,
         appBar: AppBar(
           backgroundColor: Colors.blue,
-          // title: Text("Scaffolding"),
+          title: Text("Hot Relaoding"),
+          centerTitle: true,
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('assets/two.png'),
-          ),
-        ),
+        body: const Center(child: Image(image: AssetImage('assets/two.png'))),
       ),
-    ),
-  );
+    );
+  }
 }
