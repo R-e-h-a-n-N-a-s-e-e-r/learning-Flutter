404a8c2f4b0942900624188fe30d7100be9c7088
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-14 15:43:31 +0500

    Created Simple lottery App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -13,27 +14,127 @@ class MyApp extends StatefulWidget {
 
 int x = 0;
 
+Random random = Random();
+int y = 17;
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Color(0xffdbf3ff),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: y.toString(),
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                  text: 'lucky number is ',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.grey.shade700,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: x == y
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.wb_incandescent, color: Colors.green),
+                        SizedBox(height: 20),
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            style: TextStyle(
+                              color: Colors.grey.shade700,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                            children: [
+                              TextSpan(text: 'You won with '),
+                              TextSpan(
+                                text: '$x',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.error, color: Colors.red, size: 35),
+                        SizedBox(height: 20),
+                        RichText(
+                          text: TextSpan(
+                            text: 'Better next time you got \n',
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: "$x",
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+            ),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
+          child: Icon(Icons.account_balance_wallet),
           onPressed: () {
-            x++;
+            x = random.nextInt(20);
             setState(() {});
           },
         ),
         appBar: AppBar(
-          title: Text('Stateful widget'),
-          backgroundColor: Colors.cyan,
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Text(x.toString(), style: TextStyle(fontSize: 30)),
+          title: Center(
+            child: Text(
+              "Simple Lottery App",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 1.5,
+              ),
+            ),
           ),
+          backgroundColor: Colors.cyan,
         ),
       ),
     );
