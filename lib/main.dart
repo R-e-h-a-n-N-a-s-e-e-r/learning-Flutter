ad50530ac06033b4d0cde0f99d6f9f168a77d089
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-15 12:21:23 +0500

    Expanded Widget and its properties

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,76 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('Container Widget')),
+          title: Center(child: Text('Expanded Widget')),
           backgroundColor: Colors.cyan,
         ),
         body: Column(
+          spacing: 20,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 200,
+
+                    color: Colors.red,
+                    child: Center(
+                      child: Text(
+                        "Container 1",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 200,
+
+                    color: Colors.green,
+                    child: Center(
+                      child: Text(
+                        "Container 2",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 200,
+
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        "Container 3",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
             Center(
               child: Container(
                 // margin: EdgeInsets.only(right: 40,left: 60),
                 // padding: EdgeInsets.symmetric(vertical: 50,horizontal: 60),
-                transform: Matrix4.rotationZ(0.3),
+                // transform: Matrix4.rotationZ(0.3),
                 width: 200,
-                height: 150,
+                height: 100,
                 decoration: BoxDecoration(
                   color: Colors.amber,
                   borderRadius: BorderRadius.circular(10),
