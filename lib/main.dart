3f0a7a56b2bdf6f1969b7900f751127463159175
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-15 16:18:40 +0500

    Circular Avatar , Divider & SizedBox Widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,107 +17,29 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.cyan,
         ),
         body: Column(
-          spacing: 20,
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    height: 200,
-
-                    color: Colors.red,
-                    child: Center(
-                      child: Text(
-                        "Container 1",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 200,
-
-                    color: Colors.green,
-                    child: Center(
-                      child: Text(
-                        "Container 2",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 200,
-
-                    color: Colors.blue,
-                    child: Center(
-                      child: Text(
-                        "Container 3",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            SizedBox(height: 100),
+            Divider(
+              color: Colors.blue,
+              // endIndent: 40,
+              // indent: 20,
+              thickness: 2,
+            ),
+            SizedBox(height: 100, child: VerticalDivider(color: Colors.black)),
+            Divider(
+              color: Colors.blue,
+              // endIndent: 40,
+              // indent: 20,
+              thickness: 2,
             ),
             Center(
-              child: Container(
-                // margin: EdgeInsets.only(right: 40,left: 60),
-                // padding: EdgeInsets.symmetric(vertical: 50,horizontal: 60),
-                // transform: Matrix4.rotationZ(0.3),
-                width: 200,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 25,
-                      // blurStyle: BlurStyle.outer,
-                      // spreadRadius: 10,
-                    ),
-                  ],
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg',
-                    ),
-                  ),
-                  border: BoxBorder.all(color: Colors.black, width: 2),
-                  // borderRadius: BorderRadius.only(
-                  //   topRight: Radius.circular(50),
-                  //   bottomLeft: Radius.circular(50),
-                  //   bottomRight: Radius.circular(5),
-                  //   topLeft: Radius.circular(5),
-                  // ),
-                ),
-                child: Center(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
+              child: CircleAvatar(
+                // foregroundColor: Colors.red,
+                radius: 50,
+                backgroundColor: Colors.blue,
+                // backgroundImage: NetworkImage('https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg'),
+                child: Icon(Icons.account_circle),
               ),
             ),
           ],
