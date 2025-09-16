244941d34e189b3687d4200fb703251b385e8dad
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-09-16 21:13:02 +0500

    Created a Simpler version of WhatsApp UI

--- lib/home.dart
+++ lib/home.dart
@@ -1,63 +1,119 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/screen_1.dart';
 
-class Home extends StatefulWidget {
-  static const String id = 'Home';
-
-  const Home({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Drawer Navigation')),
-        backgroundColor: Colors.blue,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
-                ),
-              ),
-              accountName: Text('Rehan'),
-              accountEmail: Text('Rehan@gmail.com'),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("WhatsApp UI"),
+          backgroundColor: Colors.teal,
+          foregroundColor: Colors.white,
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelStyle: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: 20,
             ),
-            ListTile(
-              title: Text('Home'),
-              leading: Icon(Icons.home),
-              onTap: () {
-                Navigator.pushNamed(context, Home.id);
-              },
-            ),
-            ListTile(
-              title: Text('Screen1'),
-              leading: Icon(Icons.screen_lock_portrait),
-              onTap: () {
-                Navigator.pushNamed(context, Screen1.id);
-              },
+            tabs: [
+              Tab(child: Icon(Icons.camera_alt)),
+              Tab(child: Text('Chats')),
+              Tab(child: Text('Status')),
+              Tab(child: Text('Calls')),
+            ],
+          ),
+          actions: [
+            Icon(Icons.search),
+            PopupMenuButton(
+              itemBuilder: (context) => [
+                PopupMenuItem(value: '1', child: Text('New Group')),
+                PopupMenuItem(value: '2', child: Text('Setting')),
+                PopupMenuItem(value: '3', child: Text('Profile')),
+              ],
             ),
           ],
         ),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            Navigator.pushNamed(context, Screen1.id);
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => const Screen1()),
-            // );
-          },
-          child: const Text("Go to Screen_1"),
+        body: TabBarView(
+          children: [
+            Text("1"),
+            ListView.builder(
+              itemCount: 40,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: index.isEven
+                        ? AssetImage('assets/logo.png')
+                        : NetworkImage(
+                            'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg',
+                          ),
+                  ),
+                  trailing: index.isEven ? Text("3:27 pm") : Text('1:30 am'),
+                  title: index.isEven ? Text("Henry") : Text('Peter'),
+                  subtitle: index.isEven
+                      ? Text("Hey there! i am using whatsApp")
+                      : Text('Hi Sorry working right now'),
+                );
+              },
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    "Status Update",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 15,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.green, width: 3),
+                          ),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage('assets/logo.png'),
+                          ),
+                        ),
+                        title: Text("Henry"),
+                        subtitle: Text('35 minutes Ago'),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            ListView.builder(
+              itemCount: 15,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/logo.png'),
+                  ),
+                  trailing: index / 2 == 0
+                      ? Icon(Icons.call)
+                      : Icon(Icons.video_call),
+                  title: Text("Henry"),
+                  subtitle: index / 2 == 0
+                      ? Text('You missed a call')
+                      : Text('At 3:17 pm'),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_projects/home.dart';
-import 'package:flutter_projects/screen_1.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home:Home()
-      initialRoute: Home.id,
-      routes: {
-        Home.id: (context) => Home(),
-        Screen1.id: (context) => Screen1(),
-      },
+      home: HomeScreen(),
     );
   }
 }
